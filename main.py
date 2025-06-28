cf93894ff8e5686666cf494b0b642c58fee23641
Andrew Andrey <graal101@mail.ru>
2025-06-28 16:13:23 +0700

    Первый коммит: добавлен проект Flask

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Сайт визитка для учебы и практики. Для GitLab."""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
